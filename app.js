858624829b76fc57200a7fd2f2bef20cd4d61988
Sir Ubu <thesirubu@gmail.com>
2021-05-02 19:28:30 -0600

    Added begining logic to inquirer function promt.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHtml = generatePage(name, github)
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('index.html', pageHtml, err => {
+//     if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//     console.log('Portfolio completed! Check out the index.html to see the output!');
+// });
 
-    console.log('Portfolio completed! Check out the index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
