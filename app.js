4af314d9cfb249c4a5029d6cfabd8b68a1043c2c
Sir Ubu <thesirubu@gmail.com>
2021-05-02 20:08:08 -0600

    User chooses to enter about me or not.

--- app.js
+++ app.js
@@ -41,9 +41,22 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -96,7 +109,7 @@ const promptProject = portfolioData => {
         message: 'What did you build this project with? (Check all that apply)',
         choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'],
         validate: languageChoices => {
-            if (languageChoices.length > 1 ) {
+            if (languageChoices.length > 0 ) {
                 return true;
             } else {
                 console.log('Please select at least one language!');
